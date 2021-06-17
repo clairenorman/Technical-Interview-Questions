4e14c5a77cf65d9232835c4f28d530ab3c47bf71
Claire Norman <cnorman@westmonroepartners.com>
2021-06-17 12:10:42 -0700

    reverse a linked list

--- Easy/reverseLinkedList.java
+++ Easy/reverseLinkedList.java
@@ -1,11 +1,4 @@
-import java.util.LinkedList;
-
 public class reverseLinkedList {
-    static Node head;
-    public static void main(String[] args) {
-
-    }
-
     static class Node {
 
         int data;
@@ -16,5 +9,43 @@ public class reverseLinkedList {
             data = d;
             next = null;
         }
+    }
+
+    public static Node reverseList(Node head) {
+        Node prev = null;
+        Node next = null;
+        Node curr = head;
+
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+    }
+
+    public static void printList(Node node)
+    {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Node head = new Node(2);
+        head.next = new Node(4);
+        head.next.next = new Node(6);
+        head.next.next.next = new Node(8);
+        head.next.next.next.next = new Node(12);
+
+        System.out.print("here is the current linked list: ");
+        printList(head);
+        head = reverseList(head);
+        System.out.print("here is the reversed linked list: ");
+        printList(head);
     }
 }
